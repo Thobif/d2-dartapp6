0b61e33bafcc9a6ecacaeee1583c07ad836673c5
Thobif <jirayootpandee537@gmail.com>
2021-12-09 14:13:55 +0700

    initial update

---
+++ bin/dart2_dartapp6.dart
@@ -0,0 +1,5 @@
+import 'package:dart2_dartapp6/dart2_dartapp6.dart' as dart2_dartapp6;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart2_dartapp6.calculate()}!');
+}
---
+++ test/dart2_dartapp6_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart2_dartapp6/dart2_dartapp6.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
